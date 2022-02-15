47ae0c1e1ccba21c40be8b35e1235f726e781263
unknown <pericles270@gmail.com>
2022-02-15 16:22:54 -0300

    atividade-operadores

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,74 @@
+// Exercícios de interpretação de código
+
+//1-
+
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2
+// let resultado = bool1 && bool2 
+// console.log ( "a.", false)
+// const resultado = bool1 && bool2 && bool3 
+//  console.log ("b.", fasle) 
+
+//const resultado = !bool3 && (bool1 || bool2)
+//console.log (resultado) ("c.", false) "Aqui o codigo vinha da seguinte maneira: resultado = !resultado && (bool1 || bool2)" 
+//Então removi o !resultado e coloquei no lugar bool3 e deu certo"
+
+//2
+
+//  const primeiroNumero = Number(prompt("Digite um número!"))
+//  const segundoNumero = Number(prompt("Digite outro número!"))
+//  const soma = primeiroNumero + segundoNumero
+//  console.log (soma)
+
+//Exercícios de escrita de código
+
+// 1. Faça um programa que:
+    
+// a) Pergunte a idade do usuário
+    
+// b) Pergunte a idade do seu melhor amigo ou da sua melhor amiga
+    
+// c) **Imprima na console** a seguinte mensagem: "Sua idade é maior do que a do seu melhor amigo?", seguido pela
+//   resposta (`true` ou `false`)
+    
+// d) **Imprima na console** a diferença de idade (não tem problema se sair um número negativo)
+
+//ATIVIDADE:
+
+//  const idade = prompt("Digite a sua idade")
+//  const idadeDoMelhorAmigo = prompt("Quantos anos tem seu melhor amigo?")
+//  console.log (idade)
+//  console.log (idadeDoMelhorAmigo)
+//  const maisVelho =  idade === idadeDoMelhorAmigo
+// console.log ("Sua idade é maior do que a do seu melhor amigo?", maisVelho)
+// const diferençaDeIdade = idade !== idadeDoMelhorAmigo
+// console.log (diferençaDeIdade)  
+
+//2
+// const numeroPar = prompt("Digite um numero par")
+// console.log (numeroPar %2)
+// // (resultado = 15 %2 = 1)
+// // (resultado = 10 %2 = 0)
+
+//3
+
+// const idadeUsuario = prompt("Digite sua idade em anos")
+// console.log (idadeUsuario)
+
+//Daqui pra frente não conseguir fazer.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
