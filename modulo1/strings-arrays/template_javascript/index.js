986f573669f8c8397fc1929d175b5f0bba617318
unknown <pericles270@gmail.com>
2022-02-16 16:58:02 -0300

    Atividade-strings-arrays

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,102 @@
+// 1. Indique todas as mensagens impressas no console
+// let array = ['Bahia', 'Fortaleza', 'São Paulo', 'Rio de Janeiro']
+// console.log ('Bahia', array)
+// array = null 
+// console.log (array)
+
+// const array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log (array.length) //11
+
+// let i= 0
+//  console.log (Array, [i])
+
+// const array = [i+1] = 19
+// console.log 
+
+// const array = valor [i+6]
+// console.log (array)
+
+//2. Leia o código abaixo com atenção
+
+// const frase = prompt("Digite uma frase") 
+// console.log (frase.toUpperCase() .replaceAll("A", "I"), frase.length)
+//Qual será o valor impresso no console se a entrada do usuário for: "Subi num ônibus em Marrocos"? = 27
+
+//------------------------------------------------------------------------------------------------------------\\
+
+//1. Faça um programa que pergunte ao usuário seu nome e seu e-mail. 
+//Em seguida, Imprima no console a seguinte mensagem:
+
+//  const nome = prompt('Digite seu nome')
+//  const eMail = prompt('Digite seu e-mail')
+//  console.log (nome)
+//  console.log (eMail)
+// const fraseDeConfirmação= (`O e-mail do ` + eMail + ' foi cadastrado, com sucesso!' + ' Seja bem-vindo ' + nome + '! ')
+// console.log (fraseDeConfirmação)
+
+//2. Faça um programa que contenha um array com 5 das suas comidas preferidas, 
+//armazenado em uma variável. Em seguida, siga os passos:
+
+// const comidasPreferidas = ["feijão", "macarrão", "strogonoff", "burger King", "carne"]
+
+// a) Imprima no console o array completo
+
+// console.log (comidasPreferidas)
+
+// b) Imprima no console a mensagem "Essas são as minhas comidas preferidas: ", seguida por cada uma das comidas, 
+// **uma embaixo da outra**.
+
+// const mensagem = "Essas são as minhas comidas preferidas"
+// const array = [0, 1, 2, 3, 4, 5]
+
+// console.log ("feijão", Array [0])
+// console.log ("macarrão", Array [1])
+// console.log ("strogonoff", Array [2])
+// console.log ("burger King", Array [3])
+// console.log ("carne", Array [4])
+
+// // c) Aqui vai um desafio: pergunte ao usuário uma comida preferida. Troque a segunda comida da sua lista pela inserida
+// //  pelo usuário. Imprima no console nova lista
+// const pergunte = prompt("Escolha um numero de 1 a 5")
+// console.log (pergunte)
+
+// const resultado = comidasPreferidas [pergunte -1]
+// console.log (resultado)
+
+
+//----------------------------------------------------------------------------------------------------------\\
+
+// 3. Faça um programa, seguindo os passos: 
+    // a) Crie um array vazio e guarde-o em uma variável, chamada `listaDeTarefas`
+    
+// const listaDeTarefas = []
+
+//     // b) Pergunte ao usuário 3 tarefas que ele precise realizar no dia e armazene-as, uma por uma, no array
+
+//     const    resposta1 = (prompt('Diga uma das suas atividades diarias'))
+//                 const   resposta2 = (prompt('Diga uma  segunda atividades diarias'))
+// const resposta3 = (prompt('Diga uma terceira atividades diarias'))
+//          listaDeTarefas.push (resposta1)
+//                         listaDeTarefas.push (resposta2)
+//     listaDeTarefas.push (resposta3)
+    
+//     // c) Imprima o array no console
+    
+//     console.log (listaDeTarefas)
+
+//     // d) Peça ao usuário que digite o **índice** de uma tarefa que ele já realizou: 0, 1 ou 2 
+    
+//     const repostaDoUsuario = prompt("Diga qual função já fez de 1 a 3") 
+
+//     // e) Remova da lista o item de índice que o usuário escolheu.
+
+//     listaDeTarefas.splice(( repostaDoUsuario -1), 1)
+    
+//     // f) Imprima o array no console
+
+//     console.log (listaDeTarefas)
+
+
+
+
+
